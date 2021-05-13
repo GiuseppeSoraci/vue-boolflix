7ea29d272ab8cf6965d4f3f5c8b9593fef06d9d1
GiuseppeSoraci <78480251+GiuseppeSoraci@users.noreply.github.com>
2021-05-13 11:16:53 +0200

    Milestone 3

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <img
+      v-if="details.poster_path"
+      :src="`https://image-tmdb.org/t/p/w342${details.poster_path}`"
+    />
+    <img
+      v-else
+      src="https://www.altavod.com/assets/images/poster-placeholder.png"
+    />
+
     <ul>
       <li>{{ details.title ? details.title : details.name }}</li>
       <li>
@@ -17,7 +26,18 @@
         />
         <span v-else>{{ details.original_language }}</span>
       </li>
-      <li>{{ details.vote_average }}</li>
+      <li>
+        <i
+          v-for="i in getStarVote(etails.vote_average)"
+          :key="`full-${i}`"
+          class="fas fa-star"
+        ></i>
+        <i
+          v-for="i in 5 - getStarVote(details.vote_average)"
+          :key="`empty-${i}`"
+          class="far fa-star"
+        ></i>
+      </li>
     </ul>
   </div>
 </template>
@@ -36,6 +56,9 @@ export default {
   methods: {
     isFlag(lang) {
       return this.flagsImg.includes(lang);
+    },
+    getStarVote(vote) {
+      return Math.cell(vote / 2);
     },
   },
 };
